85d5aa7d2f6890acc76adc08d59b94d2f459a910
Joshua De Matas <dematasjoshua@hotmail.com>
2025-05-20 16:10:00 +0200

    feat: added hidden ATS keywords

---
+++ KeyWords.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "@react-pdf/renderer"
+import keywords from "@assets/ats_keywords.json" with {type:"json"}
+function HiddenKeyWords() {
+    const keywordsStr = keywords.join(", ")
+    console.log(keywordsStr)
+    return (
+        <View
+            style={{
+                display: "none",
+                opacity: 0,
+            }}
+        >
+            <Text>{keywordsStr}</Text>
+        </View>
+    )
+}
+
+export { HiddenKeyWords }
--- main.tsx
+++ main.tsx
@@ -4,11 +4,13 @@ import { Document, Page, render, View } from "npm:@react-pdf/renderer"
 import { Header } from "./first_page/header.tsx"
 import { Body } from "./first_page/body.tsx"
 import { SecondPage } from "./second_page/index.tsx"
+import { HiddenKeyWords } from "@src/KeyWords.tsx"
 
 function CVDocument() {
     return (
         <Document style={{ fontFamily: "manrope" }}>
             <Page size="A4">
+                <HiddenKeyWords />
                 <Header />
                 <View style={{ flexGrow: 1 }}>
                     <Body />
