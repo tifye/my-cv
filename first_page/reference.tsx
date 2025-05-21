2f369313f92e6dc359f073132adc339302ab6636
tifufu <dematasjoshua@hotmail.com>
2025-05-21 22:35:35 +0200

    feat: removed commented out reference code

--- first_page/reference.tsx
+++ first_page/reference.tsx
@@ -21,18 +21,11 @@ type ReferenceBlockProps = {
 
 function ReferenceBlock({ reference }: ReferenceBlockProps) {
     const theme = useTheme()
-    // const links = Object.entries(reference.links)
-    //     .filter((entry) => {
-    //         return entry[1] !== undefined
-    //     })
-    //     .map((entry) => {
-    //         return { text: entry[0], link: entry[1]! }
-    //     })
     return (
         <View
             style={{
                 display: "flex",
-                gap: theme.SpacingSmall,
+                gap: theme.SpacingExtraSmall,
             }}
         >
             <Text
@@ -43,45 +36,7 @@ function ReferenceBlock({ reference }: ReferenceBlockProps) {
             >
                 {reference.name}, {reference.title}
             </Text>
-            <View
-                style={{
-                    display: "flex",
-                    gap: 2,
-                }}
-            >
-                {/* <MinWidthLink
-                    src={`mailto:${reference.email}`}
-                    style={{
-                        color: theme.Primary,
-                        fontWeight: "semibold",
-                    }}
-                >
-                    {reference.email}
-                </MinWidthLink>
-                {links.map(({ text, link }) => (
-                    <MinWidthLink
-                        src={link}
-                        style={{
-                            color: theme.Primary,
-                            fontWeight: "semibold",
-                        }}
-                    >
-                        {text}
-                    </MinWidthLink>
-                ))}
-                {reference.phone && (
-                    <MinWidthLink
-                        src={reference.phone.link}
-                        style={{
-                            color: theme.Primary,
-                            fontWeight: "semibold",
-                        }}
-                    >
-                        {reference.phone.text}
-                    </MinWidthLink>
-                )} */}
-                <Text>{reference.note}</Text>
-            </View>
+            <Text>{reference.note}</Text>
         </View>
     )
 }
